68085830d2b1114b4a2b30b51a01e651d5e9026d
a.maretskij <maretskij.a+g@gmail.com>
2023-12-10 19:32:03 +0100

    UA

--- AutoTest3/src/main/java/po/HistoryPage.java
+++ AutoTest3/src/main/java/po/HistoryPage.java
@@ -4,7 +4,7 @@ import base.mathods.BaseMethods;
 import org.openqa.selenium.By;
 
 public class HistoryPage extends BaseMethods  {
-    //Coment
+
     private static final By firstHeadingHistory = By.xpath("//h1[@class='firstHeading mw-first-heading']");
 
 
